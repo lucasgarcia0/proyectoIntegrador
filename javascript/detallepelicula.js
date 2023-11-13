f20ffb439b53ac0512a55408b51ce80717783401
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2023-11-13 19:02:57 -0300

    js

--- javascript/detallepelicula.js
+++ javascript/detallepelicula.js
@@ -12,14 +12,29 @@ fetch(`https://api.themoviedb.org/3/movie/${ID}?api_key=${api_key}`)
     })
     .then(function(data){
         console.log(data)
-        let nombrePelicula = document.querySelector(".titulo_portada_spiderman")
+        let nombrePelicula = document.querySelector(".detalle_pelicula_spiderman")
         nombrePelicula.innerHTML +=
-                `<article class="titulo_spiderman">
-                    <h2> ${data.title}</h2>
-                </article>
-                <article class="portada_spiderman">
-                    <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}">
-                </article>`
+                `<div class="titulo_portada_spiderman">
+                    <article class="titulo_spiderman">
+                        <h2> ${data.title}</h2>
+                    </article>
+                    <article class="portada_spiderman">
+                        <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}">
+                    </article>
+                </div>
+                <article class="informacion_spiderman">
+                <p> Rating: ${data.vote_average}</p>
+                <p> Fecha de estreno: ${data.release_date}</p>
+                <p> Duracion: ${data.runtime} minutos </p>
+                <p> Sinopsis: Peter Parker (Tobey Maguire) y M.J. (Kirsten Dunst) parecen estar finalmente en el camino correcto en su complicada relación, pero se avecinan problemas para el superhéroe y su amante. El traje de Spider-Man de Peter se vuelve negro y toma el control de él, no solo dándole a Peter un mayor poder sino también sacando a relucir el lado oscuro de su personalidad. Peter debe superar la influencia del traje mientras dos supervillanos, Sandman y Venom, se levantan para destruirlo a él y a todos sus seres queridos.</p>
+                <a href="./detalle_genero.html"> Genero: Acción</a>
+                    <div class="boton_favoritos_spiderman">
+                        <a href="./favorite.html"> Favoritos: </a>
+                        <input type="checkbox" name="favoritos" value=""> 
+                    </div>
+
+            </article> `
+        
 
 
 
