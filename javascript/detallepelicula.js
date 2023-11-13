e2564838083e4040e755ba145699c2d7d5d4317a
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2023-11-13 18:18:20 -0300

    detallepeliculajs

--- javascript/detallepelicula.js
+++ javascript/detallepelicula.js
@@ -1,11 +1,20 @@
 let api_key = 'adaebcb9e821851ba063cc72531a85e7'
 
+let string = location.search
+let data = new URLSearchParams(string)
 
-fetch(`https://api.themoviedb.org/3/movie/${id_pelicula}?api_key=${api_key}`)
+let ID = data.get("id")
+
+
+fetch(`https://api.themoviedb.org/3/movie/${ID}?api_key=${api_key}`)
     .then(function(response){
         return response.json()
     })
     .then(function(data){
+        
+
+
+
         
     })
     .catch(function(err){
