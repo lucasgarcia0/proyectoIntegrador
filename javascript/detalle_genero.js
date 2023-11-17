d9d38d4ce8f3382f9b707547a80582a47b451cfd
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2023-11-17 15:11:50 -0300

    detalleGenerosJS

--- javascript/detalle_genero.js
+++ javascript/detalle_genero.js
@@ -6,6 +6,9 @@ let data = new URLSearchParams(queryString)
 
 let idGenero = data.get('id')
 
+
+let nombreGenero = data.get('name')
+
 fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${idGenero}&api_key=${api_key}`)
 
     .then(function(response){
@@ -17,7 +20,7 @@ fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${idGenero}&api_k
         let detalleGeneroTitulo = document.querySelector('.titulo_accion')
         detalleGeneroTitulo.innerHTML +=
             `
-            <h2>Aca va el título del genero</h2>`
+            <h2>${nombreGenero}</h2>`
         for (i = 0; i < data.results.length; i++){
             
             detalleGeneroPelis.innerHTML +=
@@ -25,6 +28,32 @@ fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${idGenero}&api_k
             <article class="pelicula_accion 1"> 
                     <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
                     <a href="./detallepelicula.html?id=${data.results[i].id}">${data.results[i].title}</a>
+                </article>
+                
+            </section>`
+            }
+        }
+    )
+    .catch(function(err){
+        console.log(err)
+    })
+
+fetch(`https://api.themoviedb.org/3/discover/tv?with_genres=${idGenero}&api_key=${api_key}`)
+
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        let detalleGeneroPelis = document.querySelector('.contenido_accion_peliculas')
+
+        for (i = 0; i < data.results.length; i++){
+            
+            detalleGeneroPelis.innerHTML +=
+            `
+            <article class="pelicula_accion 1"> 
+                    <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
+                    <a href="./detalleserie.html?id=${data.results[i].id}">${data.results[i].name}</a>
                 </article>
                 
             </section>`
