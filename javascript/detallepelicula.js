83937b857f34f90918082274be6ef371d830918c
manudoiny <mdoiny@udesa.edu.ar>
2023-11-17 17:56:56 -0300

    js

--- javascript/detallepelicula.js
+++ javascript/detallepelicula.js
@@ -7,14 +7,15 @@ let ID = data.get("id")
 
 
 fetch(`https://api.themoviedb.org/3/movie/${ID}?api_key=${api_key}`)
-    .then(function(response){
+    .then(function (response) {
         return response.json()
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data)
+
         let nombrePelicula = document.querySelector(".detalle_pelicula_spiderman")
         nombrePelicula.innerHTML +=
-                `<div class="titulo_portada_spiderman">
+            `<div class="titulo_portada_spiderman">
                     <article class="titulo_spiderman">
                         <h2> ${data.title}</h2>
                     </article>
@@ -27,7 +28,7 @@ fetch(`https://api.themoviedb.org/3/movie/${ID}?api_key=${api_key}`)
                 <p> Fecha de estreno: ${data.release_date}</p>
                 <p> Duracion: ${data.runtime} minutos </p>
                 <p> Sinopsis: ${data.overview}</p>
-                <a href="./detalle_genero.html?genres=${data.genres[0].name}"> Genero: ${data.genres[0].name}</a>
+                <a href="./detalle_genero.html?name=${data.genres[0].name}&id=${data.genres[0].id}"> Genero: ${data.genres[0].name}</a>
                     <div class="boton_favoritos_spiderman">
                         <a href="./favorite.html"> Favoritos: </a>
                         <input type="checkbox" name="favoritos" value=""> 
@@ -35,43 +36,57 @@ fetch(`https://api.themoviedb.org/3/movie/${ID}?api_key=${api_key}`)
 
             </article> `
         
-    })
-    .catch(function(err){
-        console.log(err)
-    })
+        fetch(`https://api.themoviedb.org/3/movie/${ID}/reviews?api_key=${api_key}`)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+                let reviews = document.querySelector(".informacion_spiderman")
+                if (data.results.length > 0){
+                    reviews.innerHTML += `<h4>Reviews</h4>`
+                    for (let i = 0; i < data.results.length; i++) {
+                        reviews.innerHTML += `
+                        <p class="text_review">${data.results[i].author}: ${data.results[i].content}</p>
+                        `
+                    }
+                }
+                else if (data.results.length == 0){
+                    reviews.innerHTML += `No hay reviews disponibles`
+                }
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+        
+        fetch(`https://api.themoviedb.org/3/movie/${ID}/videos?api_key=${api_key}`)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+                if (data.results.length > 0) {
+                    let trailerVideo = document.querySelector(".titulo_portada_spiderman")
+                    trailerVideo.innerHTML += `<h2 class="text_trailers">Trailer</h2>`
 
-fetch(`https://api.themoviedb.org/3/movie/${ID}/videos?api_key=${api_key}`)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        if (data.results.lenth > 0 ){
-            let trailerVideo = document.querySelector(".titulo_portada_spiderman")
-            trailerVideo.innerHTML += `<h2>Trailers y videos</h2>`
-            for (i = 0; i < data.results.length; i++)
-                trailerVideo.innerHTML +=
-                    `<iframe width="428" height="214"
-                        src="https://www.youtube.com/embed?v=${data.results[i].key}"
-                    </iframe>`
-        }
-        else if (data.results.length == 0){
-            trailerVideo.innerHTML += `<h2>No hay trailers disponibles</h2>`
-        }
-    })
-    .catch(function(err){
-        console.log(err)
-    })
+                    trailerVideo.innerHTML += `
+                        <iframe width="380" height="220" src="https://www.youtube.com/embed/${data.results[0].key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            `
+                    trailerVideo.innerHTML += `<h2 class="text_trailers">Otros trailers y videos</h2>`
+                    for (let i = 1; i < data.results.length; i++) {
 
-fetch(`https://api.themoviedb.org/3/movie/${ID}/reviews?api_key=${api_key}`)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
 
+                        trailerVideo.innerHTML += `
+                        <iframe width="380" height="220" src="https://www.youtube.com/embed/${data.results[i].key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            `
+                    }
+                }
+                else if (data.results.length == 0) {
+                    let trailerVideo = document.querySelector(".titulo_portada_spiderman")
+                    trailerVideo.innerHTML += `<h2>No hay trailers disponibles</h2>`
+                }
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
     })
-    .catch(function(err){
-        console.log(err)
-    })
-    
--- javascript/detalleserie.js
+++ javascript/detalleserie.js
@@ -26,14 +26,68 @@ fetch(`https://api.themoviedb.org/3/tv/${ID}?api_key=${api_key}`)
             <p> Rating: ${data.vote_average}</p>
             <p> Fecha de estreno: ${data.first_air_date}</p>
             <p> Sinopsis: ${data.overview}</p>
-            <a href="./detalle_genero.html?genres=${data.genres[0].name}"> Genero: ${data.genres[0].name}</a>
+            <a href="./detalle_genero.html?name=${data.genres[0].name}&id=${data.genres[0].id}"> Genero: ${data.genres[0].name}</a>
                 <div class="boton_favoritos_friends">
                     <a href="./favorite.html"> Favoritos:</a>
                     <input type="checkbox" name="favoritos" value=""> 
                 </div>
         </article>`
+
+        fetch(`https://api.themoviedb.org/3/tv/${ID}/reviews?api_key=${api_key}`)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+                let reviews = document.querySelector(".informacion_friends")
+                if (data.results.length > 0){
+                    reviews.innerHTML += `<h4>Reviews</h4>`
+                    for (let i = 0; i < data.results.length; i++) {
+                        reviews.innerHTML += `
+                        <p class="text_review">${data.results[i].author}: ${data.results[i].content}</p>
+                        `
+                    }
+                }
+                else if (data.results.length == 0){
+                    reviews.innerHTML += `No hay reviews disponibles`
+                }
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
         
+        fetch(`https://api.themoviedb.org/3/tv/${ID}/videos?api_key=${api_key}`)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+                if (data.results.length > 0) {
+                    let trailerVideo = document.querySelector(".titulo_portada_friends")
+                    trailerVideo.innerHTML += `<h2 class="text_trailers">Trailer</h2>`
+
+                    trailerVideo.innerHTML += `
+                        <iframe width="380" height="220" src="https://www.youtube.com/embed/${data.results[0].key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            `
+                    trailerVideo.innerHTML += `<h2 class="text_trailers">Otros trailers y videos</h2>`
+                    for (let i = 1; i < data.results.length; i++) {
+
+
+                        trailerVideo.innerHTML += `
+                        <iframe width="380" height="220" src="https://www.youtube.com/embed/${data.results[i].key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            `
+                    }
+                }
+                else if (data.results.length == 0) {
+                    let trailerVideo = document.querySelector(".titulo_portada_friends")
+                    trailerVideo.innerHTML += `<h2 class="text_trailers">No hay trailers disponibles</h2>`
+                }
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
     })
-    .catch(function(err){
-        console.log(err)
-    })
+        
+        .catch(function(err){
+            console.log(err)
+        })
